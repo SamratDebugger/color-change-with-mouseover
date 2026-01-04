9385e69c12d28379f6ca9bf5a0e9362d0fc0fef7
Samrat-Debugger <md721023@gmail.com>
2026-01-05 01:20:13 +0600

    color change with mouseover

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 const body = document.querySelector("body");
 const buttons = document.querySelectorAll("button");
-
 buttons.forEach(function (button) {
-  button.addEventListener("mouseover", function (event) {
-    body.style.backgroundColor = event.target.id;
+  button.addEventListener("click", function (e) {
+    body.style.backgroundColor = e.target.id;
   });
 });
